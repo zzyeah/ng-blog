a62278f9dd07fadcde3d6f646d1cbe589d43e1e4
zzyeah <541767316@qq.com>
2021-07-16 18:00:49 +0800

    add part of component

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { AvatarComponent } from './avatar/avatar.component';
 import { MenuComponent } from './menu/menu.component';
 import { RouterModule } from '@angular/router';
+import { IconComponent } from './icon/icon.component';
 
 const COMPONENTLIST = [AvatarComponent, MenuComponent];
 
 @NgModule({
   declarations: [
     AvatarComponent,
-    MenuComponent
+    MenuComponent,
+    IconComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/component/icon/icon.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+
+
+const classMap = [
+  ['home', "iconzhuye"],
+  ['success', "iconzhengque"],
+  ['error', "iconcuowu"],
+  ['close', "iconguanbi"],
+  ['warn', "iconjinggao"],
+  ['info', "iconxinxi"],
+  ['blog', "iconblog"],
+  ['code', "iconcode"],
+  ['about', "iconset_about_hov"],
+  ['weixin', "iconweixin"],
+  ['mail', "iconemail"],
+  ['github', "icongithub"],
+  ['qq', "iconsign_qq"],
+  ['arrowUp', "iconiconfonticonfonti2copy"],
+  ['arrowDown', "iconiconfonticonfonti2"],
+  ['empty', "iconempty"],
+  ['chat', "iconliuyan"],
+];
+
+@Component({
+  selector: 'app-icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.less']
+})
+export class IconComponent implements OnInit {
+  @Input() iconName: any
+  public iconClass: string
+  constructor() { }
+  
+  initClass(): void{
+    for (const item of classMap) {
+      if (item[0] === this.iconName) {
+        this.iconClass = item[1];
+      }
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnChanges(){
+    this.initClass();
+  }
+}
